296482d8a2ccfa6a4a738ab8e71f4e5da93e608d
p.stoika <p.stoika@quantumobile.com>
2022-05-11 15:34:11 +0300

    19076: resolved

--- users/models.py
+++ users/models.py
@@ -3,8 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-<<<<<<< HEAD
     profile_photo = models.ImageField(upload_to='profile_photo/', blank=True, default='1')
-=======
-    profile_photo = models.ImageField(upload_to="profile_photo/", blank=True, default='1')
->>>>>>> 1ac6e7e5c274fd1b6a1aac2f2b03ee985fe79e7d
